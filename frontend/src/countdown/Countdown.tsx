e367d4ea6196258257d2011cc9b6e700d6a2fede
Christian Scheja <christian@scheja.me>
2019-05-11 13:00:19 +0200

    chore: separating props from state

--- frontend/src/countdown/Countdown.tsx
+++ frontend/src/countdown/Countdown.tsx
@@ -11,16 +11,20 @@ import {
 import { Timer, TimerStatus } from '../../../types'
 import { CountdownRunningState } from './CountdownRunningState';
 
-export class Countdown extends Component<{ timer: Timer }> {
+export class Countdown extends Component<{ timer: Timer }, Timer> {
+    constructor(props: { timer: Timer }) {
+        super(props);
+        this.state = this.props.timer;
+    }
 
     render() {
         return <IonCard>
             <IonCardHeader>
-                <IonCardSubtitle>{this.props.timer.name}</IonCardSubtitle>
-                <IonCardTitle>{this.props.timer.remainingSecondsSinceLastStart}s</IonCardTitle>
+                <IonCardSubtitle>{this.state.name}</IonCardSubtitle>
+                <IonCardTitle>{this.state.remainingSecondsSinceLastStart}s</IonCardTitle>
             </IonCardHeader>
             <IonCardContent>
-                <CountdownRunningState state={this.props.timer.status}></CountdownRunningState>
+                <CountdownRunningState state={this.state.status}></CountdownRunningState>
             </IonCardContent>
         </IonCard>;
     }
--- frontend/src/countdown/CountdownRunningState.tsx
+++ frontend/src/countdown/CountdownRunningState.tsx
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import {
     IonButton,
-    IonIcon
+    IonIcon,
+    IonBadge
 } from '@ionic/react';
 import { TimerStatus } from '../../../types'
+import { throwStatement } from '@babel/types';
+import Log from '../common/Log';
 
-export class CountdownRunningState extends Component<{ state: TimerStatus }> {
+export class CountdownRunningState extends Component<{ state: TimerStatus }, { state: TimerStatus }> {
+    constructor(props: { state: TimerStatus }) {
+        super(props);
+        this.state = { ...this.props };
+    }
 
     render() {
-        switch (this.props.state) {
+        switch (this.state.state) {
             case 'paused':
-                return (<IonButton><IonIcon slot="start" name="play"></IonIcon>Continue</IonButton>)
+                return (<IonButton onClick={e => this.continue(e)}><IonIcon slot="start" name="play"></IonIcon>Continue</IonButton>)
             case 'running':
-                return (<IonButton><IonIcon slot="start" name="pause"></IonIcon>Pause</IonButton>)
+                return (<IonButton onClick={e => this.pause(e)}><IonIcon slot="start" name="pause"></IonIcon>Pause</IonButton>)
+            case 'expired':
+                return (<IonBadge color="danger">Timer Expired</IonBadge>)
         }
+    }
+
+    continue(e: MouseEvent) {
+        Log.trace('paused->running', 'CountdownRunningState')
+        this.setState({ state: 'running' });
+    }
+    pause(e: MouseEvent) {
+        Log.trace('running->paused', 'CountdownRunningState')
+        this.setState({ state: 'paused' });
     }
 }
