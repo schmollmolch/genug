a8fcd591417f3f9b1cad0a7f518e04a9b9e290b6
Christian Scheja <christian@scheja.me>
2019-05-12 15:36:17 +0200

    inprogress: translation

--- frontend/src/countdown/CountdownRunningState.tsx
+++ frontend/src/countdown/CountdownRunningState.tsx
@@ -8,7 +8,10 @@ import { TimerStatus } from '../../../types'
 import { throwStatement } from '@babel/types';
 import Log from '../common/Log';
 
+import { useTranslation } from "react-i18next";
+
 export class CountdownRunningState extends Component<{ state: TimerStatus }, { state: TimerStatus }> {
+
     constructor(props: { state: TimerStatus }) {
         super(props);
         this.state = { ...this.props };
@@ -21,6 +24,7 @@ export class CountdownRunningState extends Component<{ state: TimerStatus }, { s
             case 'running':
                 return (<IonButton onClick={e => this.pause(e)}><IonIcon slot="start" name="pause"></IonIcon>Pause</IonButton>)
             case 'expired':
+                // return (<IonBadge color="danger">{t('TIMER_EXPIRED')}</IonBadge>)
                 return (<IonBadge color="danger">Timer Expired</IonBadge>)
         }
     }
